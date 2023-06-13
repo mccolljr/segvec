d985fd2efa319a7d85ec69290155c3ea4bb191df
Christian Thäter <ct@pipapo.org>
2023-06-13 11:50:57 +0200

    stateful MemConfig
    - Removes the PhantomData field and allows MemConfig to have some state.
    - MemConfig can be stateless ZST's as Linear and Proportional are
    - Exponential caches the capacity
    - Benchmarks shown that Proportional won't benefit from cached capacity

--- src/mem_config.rs
+++ src/mem_config.rs
@@ -10,6 +10,8 @@ use num_integer::Roots;
 /// Configures the sizes of segments and how to index entries. `Linear`, `Proportional` and
 /// `Exponential` implement this trait.
 pub trait MemConfig {
+    fn new() -> Self;
+
     /// Called by ctors to assert that the configuration is valid.
     ///
     /// Some `MemConfig` implementations may put constraints on (const generic)
@@ -20,35 +22,39 @@ pub trait MemConfig {
     fn debug_assert_config();
 
     /// Takes the number of allocated segments, returns the total capacity.
-    fn capacity(segments: usize) -> usize;
+    fn capacity(&self, segments: usize) -> usize;
 
     /// Returns the size of the nth segment (starting at 0).
-    fn segment_size(segment: usize) -> usize;
+    fn segment_size(&self, segment: usize) -> usize;
 
     /// Translates a flat index into (segment, offset)
-    fn segment_and_offset(index: usize) -> (usize, usize);
+    fn segment_and_offset(&self, index: usize) -> (usize, usize);
 }
 
 /// Linear growth, all segments have the same (FACTOR) length.
 pub struct Linear<const FACTOR: usize = 1024>;
 impl<const FACTOR: usize> MemConfig for Linear<FACTOR> {
+    fn new() -> Self {
+        Self {}
+    }
+
     #[track_caller]
     fn debug_assert_config() {
         debug_assert_ne!(FACTOR, 0, "FACTOR must be greater than 0")
     }
 
     #[inline]
-    fn capacity(segments: usize) -> usize {
+    fn capacity(&self, segments: usize) -> usize {
         segments * FACTOR
     }
 
     #[inline]
-    fn segment_size(_segment: usize) -> usize {
+    fn segment_size(&self, _segment: usize) -> usize {
         FACTOR
     }
 
     #[inline]
-    fn segment_and_offset(index: usize) -> (usize, usize) {
+    fn segment_and_offset(&self, index: usize) -> (usize, usize) {
         (index / FACTOR, index % FACTOR)
     }
 }
@@ -56,40 +62,48 @@ impl<const FACTOR: usize> MemConfig for Linear<FACTOR> {
 /// Proportional growth, each segment is segment_number*FACTOR sized.
 pub struct Proportional<const FACTOR: usize>;
 impl<const FACTOR: usize> MemConfig for Proportional<FACTOR> {
+    fn new() -> Self {
+        Self {}
+    }
+
     #[track_caller]
     fn debug_assert_config() {
         debug_assert_ne!(FACTOR, 0, "FACTOR must be greater than 0")
     }
 
     #[inline]
-    fn capacity(segments: usize) -> usize {
+    fn capacity(&self, segments: usize) -> usize {
         segments * (segments + 1) / 2 * FACTOR
     }
 
     #[inline]
-    fn segment_size(segment: usize) -> usize {
+    fn segment_size(&self, segment: usize) -> usize {
         (segment + 1) * FACTOR
     }
 
     #[inline]
-    fn segment_and_offset(index: usize) -> (usize, usize) {
+    fn segment_and_offset(&self, index: usize) -> (usize, usize) {
         let linear_segment = index / FACTOR;
         let segment = ((8 * linear_segment + 1).sqrt() - 1) / 2;
 
-        (segment, index - Self::capacity(segment))
+        (segment, index - self.capacity(segment))
     }
 }
 
 /// Exponential growth, each subsequent segment is as big as the sum of all segments before.
 pub struct Exponential<const FACTOR: usize = 16>;
 impl<const FACTOR: usize> MemConfig for Exponential<FACTOR> {
+    fn new() -> Self {
+        Self {}
+    }
+
     #[track_caller]
     fn debug_assert_config() {
         debug_assert_ne!(FACTOR, 0, "FACTOR must be greater than 0")
     }
 
     #[inline]
-    fn capacity(segments: usize) -> usize {
+    fn capacity(&self, segments: usize) -> usize {
         if segments == 0 {
             0
         } else {
@@ -98,7 +112,7 @@ impl<const FACTOR: usize> MemConfig for Exponential<FACTOR> {
     }
 
     #[inline]
-    fn segment_size(segment: usize) -> usize {
+    fn segment_size(&self, segment: usize) -> usize {
         if segment == 0 {
             FACTOR
         } else {
@@ -107,7 +121,7 @@ impl<const FACTOR: usize> MemConfig for Exponential<FACTOR> {
     }
 
     #[inline]
-    fn segment_and_offset(index: usize) -> (usize, usize) {
+    fn segment_and_offset(&self, index: usize) -> (usize, usize) {
         let linear_segment = index / FACTOR;
 
         if linear_segment == 0 {
@@ -124,7 +138,7 @@ pub fn linear_capacity() {
     let capacities: &[usize] = &[0, 32, 64, 96, 128, 160, 192, 224];
 
     for i in 0..capacities.len() {
-        assert_eq!(Linear::<32>::capacity(i), capacities[i])
+        assert_eq!(Linear::<32>::new().capacity(i), capacities[i])
     }
 }
 
@@ -133,13 +147,13 @@ pub fn linear_segment_size() {
     let segment_sizes: &[usize] = &[1, 1, 1, 1, 1, 1, 1];
 
     for i in 0..segment_sizes.len() {
-        assert_eq!(Linear::<1>::segment_size(i), segment_sizes[i])
+        assert_eq!(Linear::<1>::new().segment_size(i), segment_sizes[i])
     }
 
     let segment_sizes: &[usize] = &[3, 3, 3, 3, 3, 3, 3];
 
     for i in 0..segment_sizes.len() {
-        assert_eq!(Linear::<3>::segment_size(i), segment_sizes[i])
+        assert_eq!(Linear::<3>::new().segment_size(i), segment_sizes[i])
     }
 }
 
@@ -157,7 +171,7 @@ pub fn linear_segment_and_offset() {
     ];
 
     for i in 1..segment_indices.len() {
-        assert_eq!(Linear::<2>::segment_and_offset(i), segment_indices[i])
+        assert_eq!(Linear::<2>::new().segment_and_offset(i), segment_indices[i])
     }
 }
 
@@ -167,14 +181,14 @@ pub fn proportional_capacity() {
     let capacities: &[usize] = &[0, 1, 3, 6, 10, 15, 21];
 
     for i in 0..capacities.len() {
-        assert_eq!(Proportional::<1>::capacity(i), capacities[i])
+        assert_eq!(Proportional::<1>::new().capacity(i), capacities[i])
     }
 
     // For FACTOR=3
     let capacities: &[usize] = &[0, 3, 9, 18, 30, 45];
 
     for i in 0..capacities.len() {
-        assert_eq!(Proportional::<3>::capacity(i), capacities[i])
+        assert_eq!(Proportional::<3>::new().capacity(i), capacities[i])
     }
 }
 
@@ -183,19 +197,19 @@ pub fn proportional_segment_size() {
     let segment_sizes: &[usize] = &[1, 2, 3, 4, 5, 6, 7, 8];
 
     for i in 0..segment_sizes.len() {
-        assert_eq!(Proportional::<1>::segment_size(i), segment_sizes[i])
+        assert_eq!(Proportional::<1>::new().segment_size(i), segment_sizes[i])
     }
 
     let segment_sizes: &[usize] = &[2, 4, 6, 8, 10, 12, 14, 16];
 
     for i in 0..segment_sizes.len() {
-        assert_eq!(Proportional::<2>::segment_size(i), segment_sizes[i])
+        assert_eq!(Proportional::<2>::new().segment_size(i), segment_sizes[i])
     }
 
     let segment_sizes: &[usize] = &[3, 6, 9, 12, 15, 18, 21, 24];
 
     for i in 0..segment_sizes.len() {
-        assert_eq!(Proportional::<3>::segment_size(i), segment_sizes[i])
+        assert_eq!(Proportional::<3>::new().segment_size(i), segment_sizes[i])
     }
 }
 
@@ -216,7 +230,10 @@ pub fn proportional_segment_and_offset() {
     ];
 
     for i in 1..segment_indices.len() {
-        assert_eq!(Proportional::<1>::segment_and_offset(i), segment_indices[i])
+        assert_eq!(
+            Proportional::<1>::new().segment_and_offset(i),
+            segment_indices[i]
+        )
     }
 
     let segment_indices: &[(usize, usize)] = &[
@@ -237,7 +254,10 @@ pub fn proportional_segment_and_offset() {
     ];
 
     for i in 1..segment_indices.len() {
-        assert_eq!(Proportional::<2>::segment_and_offset(i), segment_indices[i])
+        assert_eq!(
+            Proportional::<2>::new().segment_and_offset(i),
+            segment_indices[i]
+        )
     }
 }
 
@@ -246,7 +266,7 @@ pub fn exponential_capacity() {
     let capacities: &[usize] = &[0, 1, 2, 4, 8, 16, 32, 64];
 
     for i in 0..capacities.len() {
-        assert_eq!(Exponential::<1>::capacity(i), capacities[i])
+        assert_eq!(Exponential::<1>::new().capacity(i), capacities[i])
     }
 }
 
@@ -255,13 +275,13 @@ pub fn exponential_segment_size() {
     let segment_sizes: &[usize] = &[1, 1, 2, 4, 8, 16, 32, 64];
 
     for i in 0..segment_sizes.len() {
-        assert_eq!(Exponential::<1>::segment_size(i), segment_sizes[i])
+        assert_eq!(Exponential::<1>::new().segment_size(i), segment_sizes[i])
     }
 
     let segment_sizes: &[usize] = &[4, 4, 8, 16, 32, 64, 128, 256];
 
     for i in 0..segment_sizes.len() {
-        assert_eq!(Exponential::<4>::segment_size(i), segment_sizes[i])
+        assert_eq!(Exponential::<4>::new().segment_size(i), segment_sizes[i])
     }
 }
 
@@ -272,7 +292,10 @@ pub fn exponential_segment_and_offset() {
         &[(0, 0), (1, 0), (2, 0), (2, 1), (3, 0), (3, 1), (3, 2)];
 
     for i in 1..segment_indices.len() {
-        assert_eq!(Exponential::<1>::segment_and_offset(i), segment_indices[i])
+        assert_eq!(
+            Exponential::<1>::new().segment_and_offset(i),
+            segment_indices[i]
+        )
     }
 
     // FACTOR = 2
@@ -297,7 +320,10 @@ pub fn exponential_segment_and_offset() {
     ];
 
     for i in 1..segment_indices.len() {
-        assert_eq!(Exponential::<2>::segment_and_offset(i), segment_indices[i])
+        assert_eq!(
+            Exponential::<2>::new().segment_and_offset(i),
+            segment_indices[i]
+        )
     }
 }
 
@@ -311,18 +337,18 @@ pub fn linear() {
     // segments
     let mut sum = 0_usize;
     for i in 0..10000000000 {
-        sum += DuT::segment_size(i);
-        assert_eq!(DuT::capacity(i + 1), sum)
+        sum += DuT::new().segment_size(i);
+        assert_eq!(DuT::new().capacity(i + 1), sum)
     }
 
     // indices
     for i in 1..10000000000 {
-        let (segment, index) = DuT::segment_and_offset(i);
-        assert!(index < DuT::segment_size(segment));
+        let (segment, index) = DuT::new().segment_and_offset(i);
+        assert!(index < DuT::new().segment_size(segment));
         if index == 0 {
-            let (psegment, pindex) = DuT::segment_and_offset(i - 1);
+            let (psegment, pindex) = DuT::new().segment_and_offset(i - 1);
             assert_eq!(psegment, segment - 1);
-            assert!(pindex < DuT::segment_size(psegment));
+            assert!(pindex < DuT::new().segment_size(psegment));
         }
     }
 }
@@ -335,18 +361,18 @@ pub fn proportional() {
     // segments
     let mut sum = 0_usize;
     for i in 0..1000000000 {
-        sum += DuT::segment_size(i);
-        assert_eq!(DuT::capacity(i + 1), sum)
+        sum += DuT::new().segment_size(i);
+        assert_eq!(DuT::new().capacity(i + 1), sum)
     }
 
     // indices
     for i in 1..10000000000 {
-        let (segment, index) = DuT::segment_and_offset(i);
-        assert!(index < DuT::segment_size(segment));
+        let (segment, index) = DuT::new().segment_and_offset(i);
+        assert!(index < DuT::new().segment_size(segment));
         if index == 0 {
-            let (psegment, pindex) = DuT::segment_and_offset(i - 1);
+            let (psegment, pindex) = DuT::new().segment_and_offset(i - 1);
             assert_eq!(psegment, segment - 1);
-            assert!(pindex < DuT::segment_size(psegment));
+            assert!(pindex < DuT::new().segment_size(psegment));
         }
     }
 }
@@ -359,18 +385,18 @@ pub fn exponential() {
     // segments
     let mut sum = 0_usize;
     for i in 0..60 {
-        sum += DuT::segment_size(i);
-        assert_eq!(DuT::capacity(i + 1), sum)
+        sum += DuT::new().segment_size(i);
+        assert_eq!(DuT::new().capacity(i + 1), sum)
     }
 
     // indices
     for i in 1..10000000000 {
-        let (segment, index) = DuT::segment_and_offset(i);
-        assert!(index < DuT::segment_size(segment));
+        let (segment, index) = DuT::new().segment_and_offset(i);
+        assert!(index < DuT::new().segment_size(segment));
         if index == 0 {
-            let (psegment, pindex) = DuT::segment_and_offset(i - 1);
+            let (psegment, pindex) = DuT::new().segment_and_offset(i - 1);
             assert_eq!(psegment, segment - 1);
-            assert!(pindex < DuT::segment_size(psegment));
+            assert!(pindex < DuT::new().segment_size(psegment));
         }
     }
 }
